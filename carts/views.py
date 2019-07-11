34533f56342167a6d3e1de09b5157717e2b4bf6d
gelsonm <gelsonvamp@gmail.com>
2019-07-11 15:55:35 +0530

    fixed cart issues

--- carts/views.py
+++ carts/views.py
@@ -1,10 +1,66 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from products.models import Product
+from .models import Cart
+from django.contrib import messages
 
 def view_cart(request):
-    return render(request, "cart.html")
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    return render(request, "cart.html",{'cart':cart_obj})
 
 
 def add_to_cart(request, id):
+    #quantity_input= request.POST.get('quantity')
+    if id is not None:
+        try:
+            product_obj = Product.objects.get(id=id)
+        except Product.DoesNotExist:
+            messages.error(request, "Product not available anymore!")
+            return redirect("cart.html")
+        cart_obj, new_obj = Cart.objects.new_or_get(request)
+        if product_obj in cart_obj.products.all(): 
+            cart_obj.products.remove(product_obj)
+            added = False
+        else:
+            cart_obj.products.add(product_obj) 
+            added = True
+            messages.success(request, "Product added succesfully to cart!")
+        request.session['cart_items'] = cart_obj.products.count()
+        
+    return redirect(reverse('index'))
+
+def delete_from_cart(request, id):
+    if id is not None:
+        try:
+            product_obj = Product.objects.get(id=id)
+        except Product.DoesNotExist:
+            messages.error(request, "Product not available anymore!")
+            return redirect("cart.html")
+        cart_obj, new_obj = Cart.objects.new_or_get(request)
+        if product_obj in cart_obj.products.all(): 
+            cart_obj.products.remove(product_obj)
+            added = False
+            messages.success(request, "Product removed from cart!")
+        else:
+            messages.error(request, "Product already deleted!")
+    return redirect(reverse('index'))
+
+'''
+def cart_update(request, id):
+    product_id = id
+    quantity_input= request.POST.get('quantity')
+
+    if product_id is not None:            
+        cart_obj, new_obj = Cart.objects.new_or_get(request)
+        Entry.objects.create(cart=cart_obj, product=product_obj, quantity=quantity_input)
+        cart_obj.products.add(product_obj)            
+        added = True
+        request.session['cart_items'] = cart_obj.products.count()        
+    return render(request, "cart.html",{'cart':cart_obj})
+'''
+
+def cart_home(request):
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    return render(request, "carts/home.html",{"cart":cart_obj})
     quantity=int(request.POST.get('quantity'))
     
     cart = request.session.get('cart', {})
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 # from django.views import ListView
 from .models import Product
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 def featured_products(request):
@@ -9,6 +10,15 @@ def featured_products(request):
 
 def all_products(request):
     products = Product.objects.all()
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(products, 5)
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
     return render(request, "product-list.html", {"products": products,"text":'All'})
 
 def product_detail(request,id):
