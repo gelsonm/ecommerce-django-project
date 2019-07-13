6bb06f80b2f025c1df4da13621e70e3f29c10ece
gelsonm <gelsonvamp@gmail.com>
2019-07-13 10:13:58 +0530

    fixed order and checkout

--- checkout/models.py
+++ checkout/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 from products.models import Product
+from django.conf import settings
+User = settings.AUTH_USER_MODEL
+
+class OrderManager(models.Manager):
+    def new_or_get(self, request):
+        created = False
+        qs = self.get_queryset().filter(user=request.user)
+        if qs.count() >= 1:
+            obj = qs.first()
+        else:
+            obj = self.model.objects.create(user=request.user)
+            created = True
+        return obj, created
+
 
 class Order(models.Model):
+    user = models.ForeignKey(User, null=True, blank=True,on_delete=models.CASCADE)
     full_name = models.CharField(max_length=50, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
     country = models.CharField(max_length=40, blank=False)
@@ -9,8 +24,9 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=40, blank=False)
     street_address_1 = models.CharField(max_length=40, blank=False)
     street_address_2 = models.CharField(max_length=40, blank=False)
-    county = models.CharField(max_length=40, blank=False)
-    date = models.DateField()
+    date = models.DateField(null=True)
+
+    objects = OrderManager()
 
     def __str__(self):
         return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
@@ -22,4 +38,4 @@ class OrderLineItem(models.Model):
     quantity = models.IntegerField(blank=False)
     
     def __str__(self):
-        return "{0} {1} @ {2}".format(self.quantity, self.product.name, self.product.price)
+        return "{0} {1} @ {2}".format(self.quantity, self.product.title, self.product.price)
--- checkout/views.py
+++ checkout/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import MakePaymentForm, OrderForm
-from .models import OrderLineItem
+from .models import OrderLineItem,Order
 from django.conf import settings
 from products.models import Product
 from django.utils import timezone
-
+from django.forms.models import model_to_dict
 
 #from accounts.forms import UserLoginForm, GuestForm
 #from accounts.models import GuestEmail
@@ -22,18 +22,23 @@ from carts.models import Cart
 @login_required()
 def checkout(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
+    order_obj,created=Order.objects.new_or_get(request)
+    order_form=OrderForm(initial=model_to_dict(order_obj))
     if request.method=="POST":
         order_form = OrderForm(request.POST)
         payment_form = MakePaymentForm(request.POST)
         
-        if order_form.is_valid() and payment_form.is_valid():
+        if order_form.is_valid():
             order = order_form.save(commit=False)
             order.date = timezone.now()
             order.save()
 
             cart_obj, new_obj = Cart.objects.new_or_get(request)
             total = 0
-            for id, quantity in cart_object.items():
+            cart = model_to_dict(cart_obj)
+            '''
+            for product in cart.products.all:
+                quantity=1
                 product = get_object_or_404(Product, pk=id)
                 total += quantity * product.price
                 order_line_item = OrderLineItem(
@@ -42,65 +47,7 @@ def checkout(request):
                     quantity = quantity
                     )
                 order_line_item.save()
+            '''
     else:
         payment_form = MakePaymentForm()
-        order_form = OrderForm() 
     return render(request, "checkout.html", {'order_form': order_form,'cart_obj':cart_obj})
-'''
-
-def checkout(request):
-    cart_obj, cart_created = Cart.objects.new_or_get(request)
-    order_obj = None
-    if cart_created or cart_obj.products.count() == 0:
-        return redirect("view_cart")  
-    
-    login_form = UserLoginForm(request=request)
-    #guest_form = GuestForm(request=request)
-    address_form = AddressCheckoutForm()
-    billing_address_id = request.session.get("billing_address_id", None)
-
-    #shipping_address_required = not cart_obj.is_digital
-
-
-    shipping_address_id = request.session.get("shipping_address_id", None)
-
-    billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
-    address_qs = None
-    has_card = False
-    if billing_profile is not None:
-        if request.user.is_authenticated():
-            address_qs = Address.objects.filter(billing_profile=billing_profile)
-        order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)
-        if shipping_address_id:
-            order_obj.shipping_address = Address.objects.get(id=shipping_address_id)
-            del request.session["shipping_address_id"]
-        if billing_address_id:
-            order_obj.billing_address = Address.objects.get(id=billing_address_id) 
-            del request.session["billing_address_id"]
-        if billing_address_id or shipping_address_id:
-            order_obj.save()
-        has_card = billing_profile.has_card
-
-    if request.method == "POST":
-        "check that order is done"
-        is_prepared = order_obj.check_done()
-        if is_prepared:
-            did_charge, crg_msg = billing_profile.charge(order_obj)
-            if did_charge:
-                order_obj.mark_paid() # sort a signal for us
-                request.session['cart_items'] = 0
-                del request.session['cart_id']
-                if not billing_profile.user:
-    
-                    is this the best spot?
-                    billing_profile.set_cards_inactive()
-                return redirect("cart:success")
-            else:
-                print(crg_msg)
-                return redirect("cart:checkout")
-    context = {
-        "object": order_obj,
-        "billing_profile": billing_profile,
-    }
-    return render(request, "checkout.html", context)
-'''
